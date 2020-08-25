980bc5779d24b541c217ef5e47bdf9e9649a2069
kirillfirsov <kifirigor@gmail.com>
2020-08-25 17:16:29 +0300

    Now can create update and delete users

--- PersonalGram/Controllers/UserController.cs
+++ PersonalGram/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
@@ -11,18 +12,51 @@ namespace PersonalGram.Controllers
     {
         UserContext _userContext = new UserContext();
 
+        /// <summary>
+        /// Список пользователей
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Users()
         {
             var users = _userContext.Users.ToList();
             return View(users);
         }
 
+        /// <summary>
+        /// Редактирование пользователей
+        /// в карточке пользователя
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public ActionResult AddUser()
+        public ActionResult User(int? id)
         {
-            return View();
+            if (id == null) return HttpNotFound();
+            if (id < 0)
+            {
+                return View(new User());
+            }
+            var user = _userContext.Users.FirstOrDefault(p => p.Id == id);
+            return View(user);
+
         }
 
+        [HttpPost]
+        public new ActionResult User(User user)
+        {
+            if (user != null)
+            {
+                _userContext.Users.AddOrUpdate(user);
+                _userContext.SaveChanges();
+            }
+
+            return RedirectToAction("Users");
+        }
+
+        /// <summary>
+        /// Добавление пользователей
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult AddUser(User user)
         {
